b174c03ce74d2370c2998bcb1206034ad0d6be62
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-08-21 20:11:12 +0200

    use hook to fetch pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+import { useFetch } from './useFetch';
 import './App.css';
 
+type Pokemon = {
+  name: string;
+  sprites: {
+    front_default: string;
+    back_default: string;
+  };
+};
+
 export default function App() {
-  return <h1>useFetch</h1>;
+  const [pokemonId, setPokemonId] = useState(1);
+  const {
+    data: pokemon,
+    isLoading,
+    error,
+  } = useFetch<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+
+  if (isLoading || !pokemon) {
+    return <h1>Loading Pokemon...</h1>;
+  }
+
+  if (error) {
+    return <h1>{error.message}</h1>;
+  }
+
+  console.log(pokemon);
+
+  return (
+    <main>
+      <figure>
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <figcaption>
+          <h1>{pokemon.name}</h1>
+        </figcaption>
+      </figure>
+      <button
+        disabled={pokemonId < 2}
+        onClick={() => setPokemonId((prevId) => prevId - 1)}>
+        Prev
+      </button>
+      <button onClick={() => setPokemonId((prevId) => prevId + 1)}>Next</button>
+    </main>
+  );
 }
