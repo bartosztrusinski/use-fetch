b64b4ba38b1534a6ea24ed68ca020a276df29610
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-08-24 16:51:01 +0200

    add arrow icons for buttons

---
+++ src/components/LeftArrowIcon.tsx
@@ -0,0 +1,17 @@
+export default function LeftArrowIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={2}
+      stroke='currentColor'
+      style={{ width: '1.6rem' }}>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M15.75 19.5 8.25 12l7.5-7.5'
+      />
+    </svg>
+  );
+}
---
+++ src/components/RightArrowIcon.tsx
@@ -0,0 +1,17 @@
+export default function RightArrowIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={2}
+      stroke='currentColor'
+      style={{ width: '1.6rem' }}>
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='m8.25 4.5 7.5 7.5-7.5 7.5'
+      />
+    </svg>
+  );
+}
