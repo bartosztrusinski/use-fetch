b6c6b9ded9593e51ce954550da85543b0ff875ac
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-08-26 23:41:19 +0200

    create debounce hook

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounce<V>(value: V, delayMs: number): V {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delayMs);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delayMs]);
+
+  return debouncedValue;
+}
